5e0876299ead7b613c82466499ece5a9fee2af00
unknown <leviettrung.social@gmail.com>
2022-08-05 07:47:14 +0700

    fix more tab

--- src/App.tsx
+++ src/App.tsx
@@ -202,14 +202,14 @@ function App() {
               <FontAwesomeIcon icon={regular('comments')} size="lg" />
               <p className="text-[12px] font-normal">Confession Box</p>
             </Link>
-            {/* <button onClick={
+            <button onClick={
               () => {
                 setIsopen(!isopen)
               }
             } className="my-2 p-2 text-slate-600 hover:text-green-900 font-semibold text-center">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={"20px"} className="mx-auto fill-slate-600"><path d="M0 80C0 53.49 21.49 32 48 32H144C170.5 32 192 53.49 192 80V176C192 202.5 170.5 224 144 224H48C21.49 224 0 202.5 0 176V80zM48 176H144V80H48V176zM0 336C0 309.5 21.49 288 48 288H144C170.5 288 192 309.5 192 336V432C192 458.5 170.5 480 144 480H48C21.49 480 0 458.5 0 432V336zM48 432H144V336H48V432zM400 32C426.5 32 448 53.49 448 80V176C448 202.5 426.5 224 400 224H304C277.5 224 256 202.5 256 176V80C256 53.49 277.5 32 304 32H400zM400 80H304V176H400V80zM256 336C256 309.5 277.5 288 304 288H400C426.5 288 448 309.5 448 336V432C448 458.5 426.5 480 400 480H304C277.5 480 256 458.5 256 432V336zM304 432H400V336H304V432z" /></svg>
               <p className="text-[12px] font-normal">Khác</p>
-            </button> */}
+            </button>
           </div>
         </div>
       </div>
