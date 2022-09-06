8eb30f4ebdcbaf9511c1abd04647b5a12c07dfe1
unknown <leviettrung.social@gmail.com>
2022-09-07 02:14:06 +0700

    clear console

--- src/components/ConfessionBox/ConfessionBox.tsx
+++ src/components/ConfessionBox/ConfessionBox.tsx
@@ -184,9 +184,14 @@ export default function ConfessionBox(props: ConfessionBoxProps) {
         <div className="flex">
           {
             props.user.uid ?
-              <Link to="/u/cfsbox" className="w-1/3 block mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
-                CfsBox của bạn
-              </Link>
+              <>
+                <Link to="/u/cfsbox" className="w-1/3 block mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
+                  CfsBox của bạn
+                </Link>
+                <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
+                  Quy định
+                </Link>
+              </>
               :
               <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
                 Quy định
@@ -195,7 +200,7 @@ export default function ConfessionBox(props: ConfessionBoxProps) {
           {
             props.user.uid && props.userdata.role === "admin" ?
               <Link to="/a/cfsbox" className="w-1/3 block mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
-                Quản lý (Admin)
+                Quản lý
               </Link>
               : null
           }
