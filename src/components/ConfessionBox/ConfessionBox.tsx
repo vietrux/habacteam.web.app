a78cd9b9980a5b8455ed9767919a1b666478aa66
unknown <leviettrung.social@gmail.com>
2022-09-07 02:17:41 +0700

    clear console

--- src/components/ConfessionBox/ConfessionBox.tsx
+++ src/components/ConfessionBox/ConfessionBox.tsx
@@ -177,7 +177,7 @@ export default function ConfessionBox(props: ConfessionBoxProps) {
                 }
               } className="inline-flex bg-red-600 text-white p-2 rounded-md items-center">
                 <svg className="h-6 w-auto mr-2 mb-[-2px]" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
-                Sign in with Google
+                Đăng nhập bằng Google
               </button>}
           </div>
         </div>
@@ -188,9 +188,12 @@ export default function ConfessionBox(props: ConfessionBoxProps) {
                 <Link to="/u/cfsbox" className="w-1/3 block mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
                   CfsBox của bạn
                 </Link>
-                <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
-                  Quy định
-                </Link>
+                {
+                  props.userdata.role !== "admin" ?
+                    <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
+                      Quy định
+                    </Link>
+                }
               </>
               :
               <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
