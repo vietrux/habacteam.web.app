ca719e4fe2f862b9b86135a844a19769c75b9c6a
unknown <leviettrung.social@gmail.com>
2022-09-07 02:40:44 +0700

    fix ui

--- src/components/ConfessionBox/ConfessionBox.tsx
+++ src/components/ConfessionBox/ConfessionBox.tsx
@@ -183,18 +183,16 @@ export default function ConfessionBox(props: ConfessionBoxProps) {
         </div>
         <div className="flex">
           {
-            props.user.uid ?
+            props.user.uid && props.userdata.role !== "admin" ?
               <>
                 <Link to="/u/cfsbox" className="w-1/3 block mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
                   CfsBox của bạn
                 </Link>
-                (
-                  props.userdata.role !== "admin" ?
-                    <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
-                      Quy định
-                    </Link>
-                )
+                <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
+                  Quy định
+                </Link>
               </>
+
               :
               <Link to="/g/i/cfsbox" className="w-1/3 mx-auto py-2 bg-[#bc6cc0] text-white text-center rounded-lg shadow-lg shadow-slate-300">
                 Quy định
